a7a7e5ed6334b55d7588d4319bfafdfbb01e9dc7
CreateRemoteThread <norman.yue@owasp.org>
2015-10-11 20:24:18 +1100

    more 64bit stuff

--- phantasm64.c
+++ phantasm64.c
@@ -9,6 +9,8 @@ char *exeCmdLine = NULL;
 
 // read: http://stackoverflow.com/questions/7446887/get-command-line-string-of-64-bit-process-from-32-bit-process
 
+void handleFirstException(HANDLE hProcess,int threadId,char firstByte);
+
 int main(int argc, char **argv)
 {
 	STARTUPINFO si;
@@ -23,9 +25,31 @@ int main(int argc, char **argv)
 	memset(&de,0,sizeof(de));
 
 	int mRet = CreateProcess("test64.exe","test64.exe",NULL,NULL,FALSE,DEBUG_PROCESS + CREATE_NEW_CONSOLE,NULL,"c:\\projects\\phantasm\\",&si,&pi);
+	size_t bytes_read;
 
 	PROCESS_BASIC_INFORMATION *pbi = (PROCESS_BASIC_INFORMATION *)malloc(sizeof(PROCESS_BASIC_INFORMATION));
 	printf("* peb base = %016x\n",pbi->PebBaseAddress);
+	PEB pebBuffer;
+	ReadProcessMemory(pi.hProcess,pbi->PebBaseAddress,&pebBuffer,sizeof(PEB),&bytes_read);
+	char *ImageBaseAddress =  (char *)pebBuffer.Reserved3[1];
+	printf("* image base address according to PEB is %016x\n",ImageBaseAddress);
+
+	IMAGE_DOS_HEADER *imgDosHdr = (IMAGE_DOS_HEADER *)malloc(sizeof(IMAGE_DOS_HEADER));
+	IMAGE_NT_HEADERS *imgNtHdrs = (IMAGE_NT_HEADERS *)malloc(sizeof(IMAGE_NT_HEADERS));
+
+	ReadProcessMemory(pi.hProcess,ImageBaseAddress,imgDosHdr,sizeof(IMAGE_DOS_HEADER),&bytes_read);
+	printf("* reading IMAGE_DOS_HEADER at %016x, magic is %x\n",ImageBaseAddress,imgDosHdr->e_magic);
+
+	ReadProcessMemory(pi.hProcess,ImageBaseAddress + imgDosHdr->e_lfanew,imgNtHdrs,sizeof(IMAGE_NT_HEADERS),&bytes_read);
+	printf("* reading IMAGE_NT_HEADERS at %016x, signature is %x\n",ImageBaseAddress + imgDosHdr->e_lfanew,imgNtHdrs->Signature);
+
+	char *entryPoint = ImageBaseAddress + imgNtHdrs->OptionalHeader.AddressOfEntryPoint;
+	printf("* entry is %016x\n",entryPoint);
+
+	char firstByte = '\x00';
+	ReadProcessMemory(pi.hProcess,entryPoint,&firstByte,1,&bytes_read);
+	WriteProcessMemory(pi.hProcess,entryPoint,"\xCC",1,&bytes_read);
+	int firstException = 1;
 
 	while(continueDebugging)
 	{
@@ -37,9 +61,21 @@ int main(int argc, char **argv)
 				continueDebugging = 0;
 				break;
 			case EXCEPTION_DEBUG_EVENT:
-				printf("* %016x\n",(DWORD64 )de.u.Exception.ExceptionRecord.ExceptionAddress);
-				ContinueDebugEvent (de.dwProcessId, de.dwThreadId,DBG_EXCEPTION_NOT_HANDLED);
-				ExitProcess(0);
+				if (firstException == 1 && de.u.Exception.ExceptionRecord.ExceptionAddress == entryPoint)
+				{
+					handleFirstException(pi.hProcess,de.dwThreadId,firstByte);
+					firstException = 0;
+				}
+				else if (firstException == 1)
+				{
+					ContinueDebugEvent (de.dwProcessId, de.dwThreadId,DBG_EXCEPTION_NOT_HANDLED);
+				}
+				else
+				{
+					printf("* exception... %016x\n",(DWORD64 )de.u.Exception.ExceptionRecord.ExceptionAddress);
+					ContinueDebugEvent (de.dwProcessId, de.dwThreadId,DBG_EXCEPTION_NOT_HANDLED);
+					ExitProcess(0);
+				}
 				break;
 			default:
 				printf("* +1\n");
@@ -49,4 +85,28 @@ int main(int argc, char **argv)
 	}
 
 	return 0;
+}
+
+// remove first exception
+void handleFirstException(HANDLE hProcess,int threadId,char firstByte)
+{
+	HANDLE hThread = OpenThread(THREAD_GET_CONTEXT | THREAD_SET_CONTEXT,FALSE,threadId);
+	
+	CONTEXT c;
+	memset(&c,0,sizeof(CONTEXT));
+	c.ContextFlags = CONTEXT_FULL;
+
+	GetThreadContext(hThread,&c);
+
+	// 32-bit i Eip here - but it's always one byte because we write \xcc
+	c.Rip -= 1;
+
+	printf("* restoring...\n");
+	size_t bytes_written;
+	WriteProcessMemory(hProcess,(LPVOID )c.Rip,&firstByte,1,&bytes_written);
+
+	SetThreadContext(hThread,&c);
+	
+	CloseHandle(hThread);
+	return;
 }
